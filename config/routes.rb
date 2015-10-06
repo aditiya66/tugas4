617ac2d27f8c1f91d47e266d9317cc7d8c1b0fad
Aditiya_Nugraha <aditiyanugraha56@gmail.com>
2015-10-06 10:27:04 +0700

    revisi

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-  get 'images/index'
+  # get 'images/index'
 
-  get 'images/new'
+  # get 'images/new'
 
-  get 'images/edit'
+  # get 'images/edit'
 
-  get 'images/show'
+  # get 'images/show'
 
-  get 'images/destroy'
+  # get 'images/destroy'
   post 'public/upload_images/images'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'images#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
