091e2069eafc6e918d15b61a06c89bbc9680efaf
Aditiya_Nugraha <aditiyanugraha56@gmail.com>
2015-10-08 07:08:47 +0700

    revisi akhir insyaalloh

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,21 +1,21 @@
 class Image < ActiveRecord::Base
 
-  has_attached_file :avatar, styles: { small: "100x200", medium: "400x200>", thumb: "200x100>" , large: "600x300>"} ,url: '/upload_images/:class/:attachment/000/000/:id/:style/:filename'
-    validates_attachment_size :avatar, :in => 0.kilobytes..200.kilobytes
+	has_attached_file :avatar, styles: { small: "100x200", medium: "400x200>", thumb: "200x100>" , large: "600x300>"} ,url: '/upload_images/:class/:attachment/000/000/:id/:style/:filename'
+	validates_attachment_size :avatar, :in => 0.kilobytes..200.kilobytes
 
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 # validates_attachment_size :avatar, size: { in: 0..20.kilobytes }
 # ,url: '/upload_images/:class/000/000/:attachment/:id/:style/:filename'
 
-# validates_attachment :avatar,
-  # content_type: { content_type: ["image/jpeg", "image/gif", "image/png","image/jpg"] }
+	# validates_attachment :avatar,
+	# content_type: { content_type: ["image/jpeg", "image/gif", "image/png","image/jpg"] }
 
 
 
-  # has_attached_file :avatar
-  # validates_attachment_content_type :avatar, content_type: /\Aimage/
-  # validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
-  # do_not_validate_attachment_file_type :avatar
+	# has_attached_file :avatar
+	# validates_attachment_content_type :avatar, content_type: /\Aimage/
+	# validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
+	# do_not_validate_attachment_file_type :avatar
 
 
 #   text_plain = MIME::Types["text/plain"].first
